21e3038eb9d633e272990d39396e5beccc409764
Long Nguyen <nguyenhailong253@gmail.com>
2022-10-29 22:31:38 +1100

    download new agent headers

--- src/common/user_agent_rotator.py
+++ src/common/user_agent_rotator.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 from itertools import cycle
 
-from .constants import DEFAULT_USER_AGENT
+from constants import DEFAULT_USER_AGENT
 
 # Rotate user agent https://www.scrapehero.com/how-to-fake-and-rotate-user-agents-using-python-3/
 # Rotate IP address and user agent https://medium.com/geekculture/rotate-ip-address-and-user-agent-to-scrape-data-a010216c8d0c
@@ -30,35 +30,51 @@ def get_random_user_agent() -> dict[str, str]:
 
 def download_agent_headers(to_csv=True):
     print("downloading free agent-headers....")
-    browers = ['firefox', 'chrome', 'facebook-app', 'android-webview',
-               'instagram', 'opera', 'edge', 'uc-browser', 'webkit-based-browser']
-    url = "https://developers.whatismybrowser.com/useragents/explore/software_name/"
+    browers = [
+        'software_name/firefox/',
+        'software_name/chrome/',
+        'software_name/facebook-app/',
+        'software_name/android-webview/',
+        'software_name/instagram/',
+        'software_name/opera/',
+        'software_name/edge/',
+        'software_name/uc-browser/',
+        'software_name/webkit-based-browser/',
+        'operating_system_name/ios/',
+        'operating_system_name/windows/',
+        'operating_system_name/linux/',
+        'operating_system_name/macos/',
+        'operating_system_name/mac-os-x/',
+        'operating_system_name/fire-os/',
+        'operating_system_name/symbian/',
+        'operating_system_name/chrome-os/'
+    ]
+    url = "https://developers.whatismybrowser.com/useragents/explore/"
 
     dfs = []
     for b in browers:
         print("downloading {0}".format(b))
         endpoint = "".join([url, b])
-        page = requests.get(endpoint, headers=DEFAULT_USER_AGENT, timeout=5)
-        print("Done request")
-        table = pd.read_html(page.text)
-        df = table[0]
-        df.columns = df.columns.str.strip()
-        dfs.append(df)
 
-    os = ['ios', 'windows', 'linux', 'macos',
-          'mac-os-x', 'fire-os', 'symbian', 'chrome-os']
-    url = "https://developers.whatismybrowser.com/useragents/explore/operating_system_name/"
-
-    for o in os:
-        print("downloading {0}".format(o))
-        endpoint = "".join([url, o])
-        page = requests.get(endpoint, headers=DEFAULT_USER_AGENT)
-        print("Done request")
-        table = pd.read_html(page.text)
-        df = table[0]
-        df.columns = df.columns.str.strip()
-        dfs.append(df)
+        for page_num in range(1, 10):
+            url_with_page = "".join([endpoint, str(page_num)])
+            print("URL: {0}".format(url_with_page))
+            try:
+                page = requests.get(
+                    url_with_page, headers=DEFAULT_USER_AGENT, timeout=5)
+            except Exception as e:
+                print("Time out exception: {0}".format(e))
+                continue
+            print("Done request")
+            table = pd.read_html(page.text)
+            df = table[0]
+            df.columns = df.columns.str.strip()
+            dfs.append(df)
 
     results = pd.concat(dfs)
     if to_csv:
         results.to_csv("user_agents.csv")
+
+
+if __name__ == "__main__":
+    download_agent_headers()
